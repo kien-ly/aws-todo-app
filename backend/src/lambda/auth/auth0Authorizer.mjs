9fc0584b3bdc0ca210a6814ff998c4a3b514af07
neikyllykien <neikyl666@gmail.com>
2024-08-16 03:18:11 +0700

    update publickey

--- backend/src/lambda/auth/auth0Authorizer.mjs
+++ backend/src/lambda/auth/auth0Authorizer.mjs
@@ -57,18 +57,17 @@ async function verifyToken(authHeader) {
     key => key.use === 'sig' && key.kid === kid && key.kty === 'RSA' && key.n && key.e
   );
 
-  // if (!signingKeys.length) {
-  //   throw new Error('The JWKS endpoint did not contain any signing keys');
-  // }
+  const publicKey = `-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA7VpF9zYzwNztZ3HL/5uo
+hn7vRcSdrMo6ZrRR7nH5uG+f3P7w7TbQGbk9XNRCafPhLxW8fRUEYfbo9Rb+UGMl
+kHJjEB5v/Y6XPQu0GJdOEJodWhL+PmeADoY+7BLnFTxLgCBX1Zz1S2uWVezgZtm7
+MEbMg5EcHQyFZRLgjsTWFi+BfnPpFo6kUiwR14kFPOQzduQ1nXnL5OXz+I6Ba8vg
+Z7tY36Bu+gXVFHYxxkFepco/bsUC9RzBhPTHHq7lmkflL/+A7DgdpMk8jbFxz/oE
+YZweGKhhn5ZK5XtTOsPyqrcFRH2sbGfL4GeT+4Wq5o3g9/xXkxUdWkmwIDAQAB
+-----END PUBLIC KEY-----`;
 
-  // const { x5c, n, e } = signingKeys[0];
-  // const publicKey = x5c
-  //   ? `-----BEGIN CERTIFICATE-----\n${x5c[0]}\n-----END CERTIFICATE-----`
-  //   : jsonwebtoken.JWKToPEM({ kty: 'RSA', n, e });
-
-  const publicKey = `-----BEGIN CERTIFICATE-----\nMIIDHTCCAgWgAwIBAgIJMyXjQrS7On73MA0GCSqGSIb3DQEBCwUAMCwxKjAoBgNVBAMTIWRldi12OHJ6djYxOHdwZnRkemtsLnVzLmF1dGgwLmNvbTAeFw0yNDA4MDQwMjUyNDNaFw0zODA0MTMwMjUyNDNaMCwxKjAoBgNVBAMTIWRldi12OHJ6djYxOHdwZnRkemtsLnVzLmF1dGgwLmNvbTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAOddZZaA8QKoHzcVi/bCCT2OFIvQyQg/8DAxyCbxE3xTyo8oCengb93sq9C/cX8+vDZfz/6TYyYVho7KYRv2wIKx+iKwYObWAomdNjupS9/yhD3zPPs+vRaRmG4tW4G3Zvhsd25XT93CehbG88nc1ST1rs7z5nGVuijyvFwVdqiOSA8Z1haByHdYBwakxwuYhMHmy6iTfQVubxE69wdufqDzWBMATslzDxx3yes9+3/pb1nq7FZAOoycCs/Oh87rx3RUZEVRhtfmsAymiFU0TDSJoDXXt1LE8D6vk6PadW1GAqkDoupGHwneEe1LeIpPVGJqHaiSBOH68J8DUyrNefsCAwEAAaNCMEAwDwYDVR0TAQH/BAUwAwEB/zAdBgNVHQ4EFgQU+So01yE8m8DeExF+bynGWfm8uqcwDgYDVR0PAQH/BAQDAgKEMA0GCSqGSIb3DQEBCwUAA4IBAQCUKekMDaFjPVuIMxFyfwD3SiCesOWORrd2KXl6Ffytpn2xBl2PrlXQxqE+upFcNDFI43VYtV2vdBb1pMDfeSMU+QVULGpE6owaG4G7diDqcK4f52U2P/9cyEfsxcpbqgh2s5HtgXI+ckVyWHBJbGTWMYHiu3qSH6WWkH9Glpm55zYBpRzvb2kLhiFJlpYDlrNmtJiRt7JN/OYb2iJjw0esQm5loGodKpu5Eyog6RQL+3PNPappLxsECuaDyXMGY/8ZKDqrpsWoiPKrvoUmngLLdE6FLK4S5yOgfRMXKk/SV8HJDdJrn8L/v76E6rfVHVcrfryGgG2oCu3lQE8C4MRC\n-----END CERTIFICATE-----`;
+  
   return jsonwebtoken.verify(token, publicKey, { algorithms: ['RS256'] });
-}
 
 
 function getToken(authHeader) {
